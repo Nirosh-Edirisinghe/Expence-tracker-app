3dc6634f11550ad6493c022c93a51ad70c929ee0
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2026-03-24 22:50:12 +0530

    create routes for incomes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import { connectDb } from './config/db.js';
 import userRouter from './routes/userRoiter.js';
+import incomeRouter from './routes/incomeRoutes.js';
 
 const app = express();
 const port = 4000
@@ -17,6 +18,7 @@ connectDb();
 
 // Routes
 app.use("/api/user", userRouter);
+app.use("/api/income", incomeRouter);
 
 app.get('/', (req, res) => {
   res.send("Api Working!")
